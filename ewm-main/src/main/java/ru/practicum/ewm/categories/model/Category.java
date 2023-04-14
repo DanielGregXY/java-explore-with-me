f904f39d116d921670f02f62eaaa495938b26113
romkery <romkery@gmail.com>
2023-04-14 23:26:28 +0700

    fix revue 3

--- ewm-main/src/main/java/ru/practicum/ewm/categories/model/Category.java
+++ ewm-main/src/main/java/ru/practicum/ewm/categories/model/Category.java
@@ -14,7 +14,6 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "categories")
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
